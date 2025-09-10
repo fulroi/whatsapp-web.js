dc2e65aa924e03afcfc18aa6a1ecd35400963bcd
Jose Luis Salinas <joseluissalinas2001@gmail.com>
2025-09-10 10:25:20 -0500

    [Parche] Autologout

--- src/util/Injected/Store.js
+++ src/util/Injected/Store.js
@@ -229,5 +229,6 @@ exports.ExposeStore = () => {
 
     window.injectToFunction({ module: 'WAWebE2EProtoUtils', function: 'typeAttributeFromProtobuf' }, (func, ...args) => { const [proto] = args; return proto.locationMessage || proto.groupInviteMessage ? 'text' : func(...args); });
 
-    window.injectToFunction({ module: 'WAWebLid1X1MigrationGating', function: 'Lid1X1MigrationUtils.isLidMigrated' }, () => false);
+    // TODO: Check if the library resolved this.
+    // window.injectToFunction({ module: 'WAWebLid1X1MigrationGating', function: 'Lid1X1MigrationUtils.isLidMigrated' }, () => false);
 };
